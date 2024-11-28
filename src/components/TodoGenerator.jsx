d5ab8efc9581c5ab94c077c064d2be5216627493
CHENJO20 <CHENJO20@oocl.com>
2024-11-28 20:15:46 +0800

    feat, will not add empty todo

--- src/components/TodoGenerator.jsx
+++ src/components/TodoGenerator.jsx
@@ -1,14 +1,18 @@
 import {useContext, useState} from "react";
 import {TodoContext} from "../App";
 import "./css/TodoGenerator.css"
+
 const TodoGenerator = () => {
-    const [text, setText] = useState();
-    const { dispatch } = useContext(TodoContext);
+    const [text, setText] = useState("");
+    const {dispatch} = useContext(TodoContext);
     const handleChange = (event) => {
         setText(event.target.value);
     }
     const handleAdd = () => {
-        dispatch({type:"ADD", payload: text});
+        if (text.trim().length === 0) {
+            return;
+        }
+        dispatch({type: "ADD", payload: text});
     }
     return (
         <div className={"todo-generator-wrapper"}>
