b12f089aad365a5f5afdb84feb3d821473fed3ef
CHENJO20 <CHENJO20@oocl.com>
2024-11-28 20:02:01 +0800

    feat, add placeHolder when there is no todo

--- src/components/TodoGroup.jsx
+++ src/components/TodoGroup.jsx
@@ -4,11 +4,15 @@ import Todo from "./Todo";
 
 const TodoGroup = () => {
     const {state} = useContext(TodoContext)
+    const placeHolderWhenNoTodo = "Add the things you need to do today..."
     return (
         <div>
-            {state.map((todo, _)=>{
-                return <Todo key={todo.id}  todo={todo}/>
-            })}
+            {
+                state.size > 0 ?
+                    state.map((todo, _) => {
+                        return <Todo key={todo.id} todo={todo}/>
+                    }) : <h5 style={{margin: "5px"}}>{placeHolderWhenNoTodo}</h5>
+            }
         </div>
     )
 }
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -2,14 +2,14 @@ import TodoGroup from "./TodoGroup";
 import TodoGenerator from "./TodoGenerator";
 
 const TodoList = () => {
-  return (
-      <div>
-          <h2>Todo List</h2>
-          <TodoGroup />
-          <TodoGenerator />
-      </div>
+    return (
+        <div>
+            <h2 style={{margin: "5px"}}>Todo List</h2>
+            <TodoGroup/>
+            <TodoGenerator/>
+        </div>
 
-  );
+    );
 }
 
 export default TodoList
