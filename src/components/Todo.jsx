f3af260b63d1b29050bed9495f3706b6e80900a4
CHENJO20 <CHENJO20@oocl.com>
2024-11-29 09:37:55 +0800

    refactor: use ES6 syntax to get todo from props

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,19 +2,19 @@ import "./css/Todo.css"
 import {TodoContext} from "../App";
 import {useContext} from "react";
 
-const Todo = (props) => {
+const Todo = ({todo}) => {
     const {dispatch} = useContext(TodoContext);
     const handleChangeDoneStatus = () => {
-        dispatch({type: "CHANGEDONESTATUS", payload: props.todo.id})
+        dispatch({type: "CHANGEDONESTATUS", payload: todo.id})
     }
     const handleRemove = () => {
-        dispatch({type: "REMOVE", payload: props.todo.id})
+        dispatch({type: "REMOVE", payload: todo.id})
     }
     return (
         <div className={"todo-wrapper"}>
             <button onClick={handleChangeDoneStatus} className={"todo-text-wrapper"}>
                 <span className={props.todo.done ? "line-through" : ""}>
-                    {props.todo.text}
+                    {todo.text}
                 </span>
             </button>
             <button onClick={handleRemove}>×</button>
