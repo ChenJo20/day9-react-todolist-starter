4f8fe483e7dc49e23f801d26a8d032ecdd2d6792
quhaibianjianshuhua <2915502829@qq.com>
2024-11-29 22:42:51 +0800

    fix, add todo but todo group will not change

--- src/components/TodoGroup.jsx
+++ src/components/TodoGroup.jsx
@@ -1,4 +1,4 @@
-import {useContext, useState} from "react";
+import {useContext, useEffect, useState} from "react";
 import {TodoContext} from "../App";
 import Todo from "./Todo";
 import {Pagination} from "antd";
@@ -6,12 +6,17 @@ import {Pagination} from "antd";
 const TodoGroup = () => {
     const {state} = useContext(TodoContext)
     const [currentPageTodos, setCurrentPageTodos] = useState(state.slice(0, 5))
+    const [currentPage, setCurrentPage] = useState(1)
+    const [currentPageSize, setCurrentPageSize] = useState(5)
     const placeHolderWhenNoTodo = "Add the things you need to do today..."
-    const onChange = (page, pageSize) => {
-        console.log(page)
+    const onPaginationChange = (page, pageSize) => {
         setCurrentPageTodos(state.slice((page - 1) * pageSize, page * pageSize))
-        console.log(currentPageTodos)
+        setCurrentPage(page)
+        setCurrentPageSize(pageSize)
     };
+    useEffect(() => {
+        setCurrentPageTodos(state.slice((currentPage-1)*currentPageSize, currentPage*currentPageSize)); // Reset to first page when state changes
+    }, [state]);
     return (
         <div>
             {
@@ -26,7 +31,7 @@ const TodoGroup = () => {
                             showTotal={(total) => `Total ${total} items`}
                             defaultPageSize={5}
                             defaultCurrent={1}
-                            onChange={onChange}
+                            onChange={onPaginationChange}
                         />
                     </div>
                     : <h5 style={{margin: "5px"}}>{placeHolderWhenNoTodo}</h5>
